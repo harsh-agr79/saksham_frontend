f200042a2103a4e3c080525091508c8e01770467
harsh-agr79 <agrharsh7932@gmail.com>
2025-07-10 18:57:26 +0530

    ai integration

--- components/chatbot.jsx
+++ components/chatbot.jsx
@@ -61,7 +61,7 @@ export const ChatBot = () => {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            model: "accounts/fireworks/models/deepseek-v3",
+            model: "accounts/fireworks/models/deepseek-r1",
             stream: false,
             messages: fullMessageHistory,
           }),
